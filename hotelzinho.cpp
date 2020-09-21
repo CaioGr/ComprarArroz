169883796d9c58fcc9a68a0908e665badb1727d9
CaioGr <71675213+CaioGr@users.noreply.github.com>
2020-09-21 20:06:46 -0300

    Add files via upload

---
+++ hotelzinho.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+    int sacoarroz = 10;
+    int dinheiro = 15;
+    
+
+int main(){
+
+    if(dinheiro >= sacoarroz){
+
+        cout << "consigo comprar saco de arroz";
+
+    }else{
+
+        cout << "nao consigo comprar saco de arroz";
+
+    }
+
+
+
+}
